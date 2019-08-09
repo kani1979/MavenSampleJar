2803470e72fafb196409cde84c6b0619daa4aa5b
kani1979 <kani1979@gmail.com>
2019-08-09 10:41:14 +0530

    initial maven version

---
+++ src/main/java/com/kani/app/App.java
@@ -0,0 +1,13 @@
+package com.kani.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
